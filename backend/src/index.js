3270b5cc375071ad2a86ad843e7cc716292c5b49
vamshi <vmarri92@gmail.com>
2025-08-24 00:16:48 +0530

    trying2

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,9 +28,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     // catch-all for React Router / SPA routes
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    app.get("/:path(*)", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
+
 }
 
 // app.get('/', (req, res) => {
