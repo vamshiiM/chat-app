e9da2665d7baa6904c5f77195bd502f60a2e73f4
vamshi <vmarri92@gmail.com>
2025-08-23 23:15:15 +0530

    finally finished

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,7 @@
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
-import ChatHeader from "./ChatHeader";
+import ChatHeader from "./Chatheader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
